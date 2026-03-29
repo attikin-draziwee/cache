7ed9b2ccded6508ec612f8e8d5a3868d12cfd6d4
Nikita Popkov <yongeryk@gmail.com>
2026-03-29 20:47:27 +0500

    feat: add struct Cache, implement base functional

--- cache.go
+++ cache.go
@@ -1,1 +1,34 @@
 package cache
+
+import "errors"
+
+type Cache struct {
+	mem map[string]any
+}
+
+func New() Cache {
+	return Cache{
+		mem: make(map[string]any),
+	}
+}
+
+func (c *Cache) Set(key string, value any) {
+	c.mem[key] = value
+}
+
+func (c Cache) Get(key string) (value any, err error) {
+	if result, err := c.mem[key]; !err {
+		return nil, errors.New(key + " not found.")
+	} else {
+		return result, nil
+	}
+}
+
+func (c *Cache) Delete(key string) (value any, err error) {
+	if result, err := c.mem[key]; !err {
+		return nil, errors.New(key + " not found.")
+	} else {
+		delete(c.mem, key)
+		return result, nil
+	}
+}
